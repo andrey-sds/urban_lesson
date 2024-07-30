1210c5b7174eb1789bc2dd273fc59c8f9cc617e2
andrey-sds <dz_andrey@mail.ru>
2024-07-30 14:00:23 +0300

    Add files via upload

--- module_3_3.py
+++ module_3_3.py
@@ -1,18 +1,22 @@
 def print_params(a=1, b='string', c=True):
-    print(f'Вывод параметров \r\na, b, c:\r\n{a} {b} {c}')
+    print(f'Значения a, b, c:\r\n{a} {b} {c}')
 
 
 values_list = ['Apple', 22, 13.5]
 values_dict = {'a': 44, 'b': False, 'c': 'BaNan'}
+print('Вывод с параметрами по умолчанию.')
 print_params()
+print('Вывод с передачей именнованого параметра.')
 print_params(b=25)
+print('Вывод с передачей именнованого параметра 2.')
 print_params(c=[1,2,3])
 
-print(f'Распаковка списка и словаря:')
+print(f'Распаковка списка.')
 print_params(*values_list)
+print(f'Распаковка словаря.')
 print_params(**values_dict)
 
 values_list2 = [4, 'Кокос']
 
-print(f'Распаковка + отдельные параметры:')
+print(f'Распаковка + отдельные параметры.')
 print_params(*values_list2, 42)
