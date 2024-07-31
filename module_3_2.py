7a786350a8d01f22f9e96fd49bba8f4416d090fa
andrey-sds <dz_andrey@mail.ru>
2024-07-31 13:15:03 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,6 +1,5 @@
 def send_mail(message, recipient, sender='university.help@gmail.com'):
-    #  if (recipient or sender not in '@') or (   # невозможно отправить
-    if ('@' not in sender and '@' not in recipient) or (  # успешная отправка
+    if ("@" not in sender or "@" not in recipient) or (
            not sender.endswith((".com", ".ru", ".net")) or not recipient.endswith((".com", ".ru", ".net"))):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
 
@@ -14,7 +13,7 @@ def send_mail(message, recipient, sender='university.help@gmail.com'):
         print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
 
 
-send_mail('Это сообщение для проверки связи', 'vasyok1337gmail.com')
+send_mail('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_mail('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_mail('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_mail('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
